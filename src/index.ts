04950f9e436af831d67d681b6a4fb5e42df08e00
Giovanni Hsu <giovannihsu.123@gmail.com>
2023-02-21 23:53:01 -0500

    set up sockets for beackend

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import cors from 'cors';
 import express, { Express, Request, Response } from 'express';
 import morgan from 'morgan';
 import login from "./routes/login"
+import { WebSocket } from 'ws';
+
+
+const app: Express = express();
 
 //* import local
 import db from './db';
 
 //* server init
-const app: Express = express();
 db.connect();
 
 //* configure middleware
@@ -28,3 +31,22 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(process.env.PORT, () => {
   console.log(`listening ${process.env.PORT}`);
 });
+
+const wss = new WebSocket.Server({ port: 4000 });
+
+wss.on('connection', function connection(ws) {
+  console.log("Connection Made")
+  ws.on('error', console.error);
+
+  ws.on('message', (data) => {
+    wss.clients.forEach((client) => {
+      client.send("Someone said: " + data)
+    });
+    console.log('received: %s', data);
+    // ws.send('something');
+  });
+});
+
+// wss.on('listening', () => {
+//   console.log(`WebSocket server listening on ws://localhost:${wss.address()}`);
+// });
